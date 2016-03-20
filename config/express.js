925d909010ff20869656b50bc32d05a6a7fda284
muvaffak <onus.muvaffak@gmail.com>
2016-03-21 00:57:08 +0200

    added log for req

--- config/express.js
+++ config/express.js
@@ -1,19 +1,23 @@
 var config = require('./config'),
-	express = require('express'),
-	bodyParser = require('body-parser');
+    express = require('express'),
+    bodyParser = require('body-parser');
 
-module.exports = function() {
-	var app = express();
+module.exports = function () {
+    var app = express();
 
-	app.use(bodyParser.urlencoded({
-		extended: true
-	}));
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }));
 
-	app.use(bodyParser.json());
+    app.use(bodyParser.json());
+    app.use('/', function (req, res, next) {
+        console.log("req came");
 
-	require('../app/routes/main.routes.js')(app);
+        next();
+    });
+    require('../app/routes/main.routes.js')(app);
 
-	app.use(express.static('./public'));
+    app.use(express.static('./public'));
 
-	return app;
+    return app;
 };
