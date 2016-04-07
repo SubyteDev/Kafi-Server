342b121b971c557ee76528403042083256370584
muvaffak <onus.muvaffak@gmail.com>
2016-04-07 23:31:31 +0300

    not kare kafi...

--- app/routes/main.routes.js
+++ app/routes/main.routes.js
@@ -2,9 +2,9 @@ var mainController = require('../../app/controllers/main.controller');
 
 module.exports = function(app){
     //TODO: add a middleware for /kare now have no time.    app.route('/kare/request').post(mainController.requestCoffee);
-    app.route('/kare/check').get(mainController.checkRequest);
-    app.route('/kare/completed').post(mainController.completeRequest);
-    app.route('/kare/received').post(mainController.requestReceived);
-    app.route('/kare/list').get(mainController.listAllRequests);
+    app.route('/kafi/check').get(mainController.checkRequest);
+    app.route('/kafi/completed').post(mainController.completeRequest);
+    app.route('/kafi/received').post(mainController.requestReceived);
+    app.route('/kafi/list').get(mainController.listAllRequests);
 
 };
