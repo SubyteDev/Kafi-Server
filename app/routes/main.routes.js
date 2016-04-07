3690a2c37aece80944b2c3f06beb08a84b105ebe
muvaffak <onus.muvaffak@gmail.com>
2016-04-07 23:28:52 +0300

    fix for middleware

--- app/routes/main.routes.js
+++ app/routes/main.routes.js
@@ -1,10 +1,10 @@
 var mainController = require('../../app/controllers/main.controller');
 
 module.exports = function(app){
-    app.route('/request').post(mainController.requestCoffee);
-    app.route('/check').get(mainController.checkRequest);
-    app.route('/completed').post(mainController.completeRequest);
-    app.route('/received').post(mainController.requestReceived);
-    app.route('/list').get(mainController.listAllRequests);
+    //TODO: add a middleware for /kare now have no time.    app.route('/kare/request').post(mainController.requestCoffee);
+    app.route('/kare/check').get(mainController.checkRequest);
+    app.route('/kare/completed').post(mainController.completeRequest);
+    app.route('/kare/received').post(mainController.requestReceived);
+    app.route('/kare/list').get(mainController.listAllRequests);
 
 };
--- config/express.js
+++ config/express.js
@@ -11,11 +11,6 @@ module.exports = function () {
 
     app.use(bodyParser.json());
     
-    app.use('/kare', function (req, res, next){
-        console.log("log: " + req.body);
-        next();
-    });
-    
     require('../app/routes/main.routes.js')(app);
 
     app.use(express.static('./public'));
