35d8760ab34f61fc0bb5b7b3f37032b5f3281f79
bymafmaf <onus.muvaffak@gmail.com>
2016-04-11 09:39:04 +0000

    typo fix

--- app/routes/main.routes.js
+++ app/routes/main.routes.js
@@ -2,7 +2,7 @@ var mainController = require('../../app/controllers/main.controller');
 
 module.exports = function(app){
     //TODO: add a middleware for kare now have no time.
-    app.route('/kare/request').post(mainController.requestCoffee);
+    app.route('/kafi/request').post(mainController.requestCoffee);
     app.route('/kafi/check').get(mainController.checkRequest);
     app.route('/kafi/completed').post(mainController.completeRequest);
     app.route('/kafi/received').post(mainController.requestReceived);
